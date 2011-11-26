0df51b8bce1b5ad324acc666fea33ed9d159b8aa
Sjors Gielen <dazjorz@dazjorz.com>
2011-11-26 16:41:44 +0100

    Add keyboard dict so keyboard buttons actually do something.

--- pymixml/KeyboardInputDevice.py
+++ pymixml/KeyboardInputDevice.py
@@ -1,5 +1,24 @@
 from pymixml.InputDevice import *
 
+_keyboard_dict = {
+	'q' : ('cue', 0),
+	'w' : ('play', 0),
+	'e' : ('rewind', 0),
+	'r' : ('forward', 0),
+	't' : ('rewind', 0),
+	'y' : ('forward', 0),
+
+	'a' : ('cue', 1),
+	's' : ('play', 1),
+	'd' : ('rewind', 1),
+	'f' : ('forward', 1),
+	'g' : ('rewind', 1),
+	'h' : ('forward', 1),
+
+	'Q' : ('quit', -1),
+}
+
+
 class KeyboardInputDevice(InputDevice):
 	def __init__(self):
 		super(KeyboardInputDevice, self).__init__()
@@ -9,7 +28,8 @@ class KeyboardInputDevice(InputDevice):
 			self.readone = _GetchWindows()
 
 	def Read(self):
-		return self.readone()
+		rawbutton = self.readone()
+		return _keyboard_dict[rawbutton]
 
 # Source: http://code.activestate.com/recipes/134892/
 class _GetchUnix:
